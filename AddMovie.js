cfc8bed12d8457ace37ec761d8d7901b5ac917f4
gaganMadan <gagan7535@gmail.com>
2020-06-30 16:27:02 +0530

    1 file modified

--- AddMovie.js
+++ AddMovie.js
@@ -2,30 +2,37 @@ import React, {useContext, useState} from 'react'
 import {MovieContext} from './MovieContext'
 
 const AddMovie = () => {
-    const [name, setName] = useState('');
-    const [cost, setCost] = useState('');
+    const [email, setEmail] = useState('');
+    const [firstName, setFirstname] = useState('');
+    const [lastName, setLastname] = useState('');
 
     const [movies,setMovies] = useContext(MovieContext);
 
-    const updateName = (e) => {
-        setName(e.target.value)
+    const updateEmail = (e) => {
+        setEmail(e.target.value)
     }
 
-    const updateCost = (e) => {
-        setCost(e.target.value)
+    const updateFirstname = (e) => {
+        setFirstname(e.target.value)
+    }
+
+    const updateLastname = (e) => {
+        setLastname(e.target.value)
     }
 
     const updateMovies = (e) => {
       e.preventDefault();
-      setMovies(prevMovies => [...prevMovies, {name:name, prise: cost}])
+      setMovies(prevMovies => [...prevMovies, {email:email, first_name: firstname, last_name: lastName}])
     }
 
 
     return (
       <form onSubmit={updateMovies}>
-          <span>Please Enter the movie Name : <input type="text" placeholder="Enter the Movie Name" value={name} onChange={updateName} /></span>
+          <span>Please Enter the movie Name : <input type="text" placeholder="Enter the Email" value={email} onChange={updateEmail} /></span>
           <br />
-          <span>Please Enter the movie Cost : <input type="text" placeholder="Enter the Movie Cost" value={cost} onChange={updateCost}/></span>
+          <span>Please Enter the First Name : <input type="text" placeholder="Enter the Movie Cost" value={firstName} onChange={updateFirstname}/></span>
+          <br />
+          <span>Please Enter the Last Name : <input type="text" placeholder="Enter the Last Name" value={lastName} onChange={updateLastname}/></span>
           <button>Submit</button>
 
       </form>
