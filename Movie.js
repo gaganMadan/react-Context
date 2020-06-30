07954278d5addc111e68c34abbb2faa4e6070aa8
gaganMadan <gagan7535@gmail.com>
2020-06-30 14:42:20 +0530

    3 files modified

--- Movie.js
+++ Movie.js
@@ -3,9 +3,10 @@ import React from 'react'
 const Movie = ({item}) => {
   return (
     <>
-      <h2>{item.name}</h2>
+      <h2>{item.email}</h2>
       
-      <h2>{item.prise}</h2>
+      <h2>{item.first_name} {item.last_name}</h2>
+      <img src={item.avatar}/>
     </>
   )
 }
--- MovieContext.js
+++ MovieContext.js
@@ -1,32 +1,45 @@
-import React, {useState, createContext} from 'react'
+import React, {useState, createContext, useEffect} from 'react'
+import axios from 'axios'
 
 
 export const MovieContext = createContext();
 
 export const MovieProvider = (props) => {
 
-  const [movies, setMovies] = useState([
-    {
-      name : 'Firsr Movie',
-      prise : '2000',
-      id : '1'
-    },
-    {
-      name : 'Second Movie',
-      prise : '1000',
-       id : '2'
-    },
-    {
-      name : 'Star Movie',
-      prise : '5000',
-       id : '3'
-    },
-    {
-      name : 'Forth Movie',
-      prise : '5000',
-       id : '4'
-    }
-  ])
+  // const [movies, setMovies] = useState([
+  //   {
+  //     name : 'Firsr Movie',
+  //     prise : '2000',
+  //     id : '1'
+  //   },
+  //   {
+  //     name : 'Second Movie',
+  //     prise : '1000',
+  //      id : '2'
+  //   },
+  //   {
+  //     name : 'Star Movie',
+  //     prise : '5000',
+  //      id : '3'
+  //   },
+  //   {
+  //     name : 'Forth Movie',
+  //     prise : '5000',
+  //      id : '4'
+  //   }
+  // ])
+
+  const [movies, setMovies] = useState(['']);
+
+  useEffect(() => {
+      axios.get('https://reqres.in/api/users?page=2')
+        .then((response) => {
+          setMovies(response.data.data)
+          //console.log(response.data.data)
+        }, (error) => {
+          console.log(error);
+        })
+  }, [])
 
 
   return (
